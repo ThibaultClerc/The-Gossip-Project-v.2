637a2159698d8aa0df679fb5afc4c7271220f4e8
ThibaultClerc <thibault.clerc2@gmail.com>
2020-08-06 01:04:09 +0200

    Adding comments ok

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,28 @@
+class CommentsController < ApplicationController
+
+  def index
+  end
+  
+  def new
+    @comment = Comment.new
+  end
+
+
+  def create
+    @id = params[:id]
+    @comment = Comment.create(content: params[:content], author_id: 11, gossip_id: params[:gossip_id])
+    redirect_to gossip_path(params[:gossip_id])
+    @comment
+  end
+
+  
+  def destroy
+  end
+
+  private
+
+  # def comment_params
+  #   params.require(:comment).permit(:content)
+  # end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,8 @@ Rails.application.routes.draw do
   resources :gossips
   resources :users
   resources :cities, only: [:show]
-
+  resources :gossips do
+    resources :comments, only: [:new, :create, :index, :destroy]
+  end
+  post '/gossips/:id/comments', to: "comments#create", as:"gossips_comment_create"
 end
