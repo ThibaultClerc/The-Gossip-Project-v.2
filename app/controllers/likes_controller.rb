cee2f8f9f848da1e3b3d8d9db7b744399e68d5a5
ThibaultClerc <thibault.clerc2@gmail.com>
2020-08-10 00:23:53 +0200

    syntax error on likes_controller ok

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,25 +3,26 @@ class LikesController < ApplicationController
   end
 
   def create
-    @user = current_user.id
-    @gossip = params[:gossip_id]
-    likes = {liker_id: @user, gossip_id: @gossip}
-    @like = Like.new(likes)
-    @like.save!
-    if @like.save
-      redirect_to gossips_path
-    else
-      render '/gossips'
-    end
+    # @user = current_user.id
+    # @gossip = params[:gossip_id]
+    # likes = {liker_id: @user, gossip_id: @gossip}
+    # @like = Like.new(likes)
+    # @like.save!
+    # if @like.save
+    #   redirect_to gossips_path
+    # else
+    #   render '/gossips'
+    # end
   end
 
   def destroy
-    get_like_by_user(current_user).destroy
-    redirect_to gossips_path
+    # get_like_by_user(current_user).destroy
+    # redirect_to gossips_path
   end
 
   private
 
-  def like_params
-    params.require(:like).permit(:gossip_id, :liker_id)
+  # def like_params
+  #   params.require(:like).permit(:gossip_id, :liker_id)
+  # end
 end
